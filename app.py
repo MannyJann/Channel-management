0fe0188f93749bb4bebe0a0d1ace6f4914a1f65b
MannyJann <69632860+MannyJann@users.noreply.github.com>
2020-12-02 16:22:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+from utils.bot_commands import set_default_commands
+
+
 async def on_startup(dp):
-    import filters
     import middlewares
-    filters.setup(dp)
     middlewares.setup(dp)
 
     from utils.notify_admins import on_startup_notify
     await on_startup_notify(dp)
+    await set_default_commands(dp)
 
 
 if __name__ == '__main__':
